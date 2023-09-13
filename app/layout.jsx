d84773d64c7decdd0fc055efbaea5a09606fc8fd
klenot <klenoticmarek@gmail.com>
2023-09-13 20:25:07 +0100

    Hydration error
    - fixed the hydration error
    
    Continue to blog post filters or global section padding.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,20 @@
-import 'styles/globals.css'
-import Head from "app/components/Shared/head.jsx"
-import NavBar from "app/components/Shared/navBar.jsx"
-import Providers from "app/providers.jsx"
+import "styles/globals.css";
+import Head from "app/components/Shared/head.jsx";
+import NavBar from "app/components/Shared/navBar.jsx";
+import Providers from "app/providers.jsx";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark-theme">
-      
+    <html lang='en' className='dark-theme'>
       <Head />
-      
-      <Providers>
-
-        <body>
-          
+      <body>
+        <Providers>
           <NavBar />
 
           {children}
-            
-        </body>
-      
-      </Providers >
-    
+          
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
