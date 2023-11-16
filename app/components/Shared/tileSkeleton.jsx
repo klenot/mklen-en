faa3e61bbea1636d0d08ff99f115b60f7cb33ec5
klenot <klenoticmarek@gmail.com>
2023-11-16 18:17:49 +0000

    More fixes
    - overall fixes

--- app/components/Shared/tileSkeleton.jsx
+++ app/components/Shared/tileSkeleton.jsx
@@ -21,6 +21,16 @@ export default function TileSkeleton() {
         </div>
       </div>
 
+      <div className='tile-wrapper'>
+        <div className='tile-skeleton-card'>
+          <div className='skeleton-image'></div>
+          <div className='skeleton-text-wrapper'>
+            <div className='skeleton-text-longer'></div>
+            <div className='skeleton-text-shorter'></div>
+          </div>
+        </div>
+      </div>
+
     </>
   );
 }
